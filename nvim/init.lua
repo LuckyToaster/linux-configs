36a5e759e98433c7aeec1010283ef01d22fdf870
lucky <me@toaster.com>
2025-03-22 18:57:26 +0100

    improved nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,84 +1,44 @@
+-- ======================================================================================================
+-- = BASIC VIM SETTTINGS ================================================================================
+-- ======================================================================================================
+
 local keymap = vim.keymap
 local opt = vim.opt
 
 -- disable netrw at the very start of your init.lua (for nvim-tree)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
+-- leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"         -- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim
-opt.splitbelow = true
-opt.splitright = true
-opt.termguicolors = true
+-- tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smarttab = true
+-- line numbers
 opt.number = true
 opt.relativenumber = true
 opt.cursorline = true               -- highlight current cursor line
-opt.signcolumn = "yes"              -- show sign column so that text doesn't shift
-opt.backspace = "indent,eol,start"  -- allow backspace on any of these
-opt.showmatch = true                -- highlight matching parenthesis
-opt.clipboard = 'unnamedplus'       -- Set up the clipboard YOU NEED TO HAVE XCLIP INSTALLED
-opt.mouse = 'a'                     -- Enable mouse support
-opt.laststatus = 2                  -- always show the status line
-opt.showtabline = 1                 -- don't always show the tab line
-opt.ruler = true                    -- show the cursor position all the time
-opt.wrap = false                    -- same as nowrap
-opt.formatoptions:remove('r')       -- stop inserting comment leader when new line on insert mode
-opt.formatoptions:remove('o')       -- stop auto wrapping comments when inserting text
-opt.scrolloff = 15                   -- when scrolling, prevent cursor from being at first or last line
-opt.sidescrolloff = 30
+-- search settings
 opt.ignorecase = true               -- case insensitive search
 opt.smartcase = true                -- assumes case sensitive if mixed case in search
-opt.tabstop = 4
-opt.shiftwidth = 4
---opt.autoindent = true
---opt.smartindent = true
-opt.expandtab = true
-opt.smarttab = true
---opt.scrollbarState = 'hidden'       -- try to prevent borders / take up all screen space
+-- clipboard, mouse support
+opt.clipboard = 'unnamedplus'       -- Set up the clipboard YOU NEED TO HAVE XCLIP INSTALLED
+opt.mouse = 'a'                     -- Enable mouse support
+-- other
+opt.laststatus = 2                  -- always show the status line
+opt.wrap = false                    -- same as nowrap
+opt.showmatch = true                -- highlight matching parenthesis
+opt.signcolumn = "yes"              -- show sign column so that text doesn't shift
+opt.backspace = "indent,eol,start"  -- allow backspace on any of these
+opt.scrolloff = 15                  -- when scrolling, prevent cursor from being at first or last line
+opt.sidescrolloff = 30
+opt.splitright = true
 
-keymap.set("i", "df", "<ESC>", { desc = "Exit insert mode with 'df'"})
-keymap.set('t', 'df', '<C-\\><C-n>', { desc = "Exit insert mode when in terminal mode" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "no highlight, clear search highlights"})
-
--- BUFFERS 
-keymap.set('n', '<leader>n', ':enew<CR>', { desc = "open a new empty buffer" })
-keymap.set('n', '<leader>w', ':bd!<CR>', { desc = "Close / delete current buffer" })
-keymap.set('n', '<leader>t', ':term<CR>', { desc = "Open a terminal buffer" })
-keymap.set('n', '<leader>l', ':bnext<CR>', { desc = "Switch to next open buffer" })
-keymap.set('n', '<leader>h', ':bprev<CR>', { desc = "Switch to previous open buffer" })
-
--- SPLITS
-keymap.set('n', '<leader>sh', ':sp | enew<CR>', { desc = "Open new buffer in a horizontal split" })
-keymap.set('n', '<leader>sv', ':vs | enew<CR>', { desc = "Open new buffer in a vertical split" })
-
-keymap.set('n', '<leader>sth', ':sp | :term<CR>', { desc = "Open terminal buffer in a horizontal split" })
-keymap.set('n', '<leader>stv', ':vs | :term<CR>', { desc = "Open terminal bufferin a  vertical split" })
-
-keymap.set('n', '<leader>ssv', '<C-w>t<C-w>H', { desc = "change 2 splits from horizontal to vertical" }) -- too complicated and unnecessary?
-keymap.set('n', '<leader>ssh', '<C-w>t<C-w>K', { desc = "change 2 splits from vertical to horizontal" }) -- too complicated and unnecessary?
-
-keymap.set('n', '<C-h>', '<C-w>h', { desc = "navigate splits with 'control + hjkl'" })
-keymap.set('n', '<C-j>', '<C-w>j', { desc = "navigate splits with 'control + hjkl'" })
-keymap.set('n', '<C-k>', '<C-w>k', { desc = "navigate splits with 'control + hjkl'" })
-keymap.set('n', '<C-l>', '<C-w>l', { desc = "navigate splits with 'control + hjkl'" })
-
-keymap.set('n', '<C-left>', ':vertical resize +4<CR>', { desc = "resize splits with 'control + arrow keys'" })
-keymap.set('n', '<C-right>', ':vertical resize -4<CR>', { desc = "resize splits with 'control + arrow keys'" })
-keymap.set('n', '<C-up>', ':horizontal resize +4<CR>', { desc = "resize splits with 'control + arrow keys'" })
-keymap.set('n', '<C-down>', ':horizontal resize -4<CR>', { desc = "resize splits with 'control + arrow keys'" })
-
-vim.api.nvim_create_autocmd("TermOpen", {       -- Disable line numbers in terminal mode
-    pattern = "*",
-    callback = function()
-        vim.opt_local.number = false            -- Disable absolute line numbers
-        vim.opt_local.relativenumber = false    -- Disable relative line numbers
-    end,
-})
-
-
--- ==================
--- = PLUGIN MANAGER =
--- ==================
+-- ======================================================================================================
+-- = PLUGIN MANAGER =====================================================================================
+-- ======================================================================================================
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -104,38 +64,67 @@ require("lazy").setup({
         {'williamboman/mason-lspconfig.nvim'},
         {'neovim/nvim-lspconfig', lazy = false},
         {'nvim-treesitter/nvim-treesitter'},
-        {'christianchiarulli/nvcode-color-schemes.vim'}, -- colorschemes that work well with tree-sitter (and depend on it as a dependency)
-        {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+        {'Mofiqul/vscode.nvim'},
+        {"ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" }}, --, opts = {}},
+        {'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}},
         {'rmagatti/auto-session', lazy = false, opts = {suppressed_dirs = {'~/Downloads', '/'}}},
         {'nvim-tree/nvim-tree.lua', lazy = false, dependencies = { 'nvim-tree/nvim-web-devicons'}},
-        {'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}},
+        {'gorbit99/codewindow.nvim'}, -- leader mm (minimap toggle) leader mf (minimap focus)
     },
-    -- Configure any other settings here. See the documentation for more details.
-    install = { colorscheme = { "nvcode" } },    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "vscode" } },    -- colorscheme that will be used when installing plugins.
     checker = { enabled = true },                -- automatically check for plugin updates
 })
 
+-- ======================================================================================================
+-- = KEYMAPS ============================================================================================
+-- ======================================================================================================
 
--- ===================
--- = PLUGIN SETTINGS =
--- ==================
-
--- COLORSCHEME => https://github.com/nvim-treesitter/nvim-treesitter/wiki/Colorschemes
-vim.cmd("colorscheme nvcode") -- vim.cmd("syntax on") will fuck up the treesitter syntax highlighting
-
--- Lualine 
-require('lualine').setup({options = {theme = 'vscode',},})
-
+keymap.set("i", "df", "<ESC>", { desc = "Exit insert mode with 'df'"})
+keymap.set('t', 'df', '<C-\\><C-n>', { desc = "Exit insert mode when in terminal mode" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "no highlight, clear search highlights"})
+-- BUFFERS 
+keymap.set('n', '<leader>n', ':enew<CR>', { desc = "open a new empty buffer" })
+keymap.set('n', '<leader>w', ':bd!<CR>', { desc = "Close / delete current buffer" })
+keymap.set('n', '<leader>t', ':term<CR>', { desc = "Open a terminal buffer" })
+keymap.set('n', '<leader>l', ':bnext<CR>', { desc = "Switch to next open buffer" })
+keymap.set('n', '<leader>h', ':bprev<CR>', { desc = "Switch to previous open buffer" })
+-- SPLITS
+keymap.set('n', '<leader>s', ':vs | enew<CR>', { desc = "Open new buffer in a vertical split" })
+keymap.set('n', '<C-h>', '<C-w>h', { desc = "navigate splits with 'control + hjkl'" })
+keymap.set('n', '<C-j>', '<C-w>j', { desc = "navigate splits with 'control + hjkl'" })
+keymap.set('n', '<C-k>', '<C-w>k', { desc = "navigate splits with 'control + hjkl'" })
+keymap.set('n', '<C-l>', '<C-w>l', { desc = "navigate splits with 'control + hjkl'" })
+keymap.set('n', '<C-left>', ':vertical resize +4<CR>', { desc = "resize splits with 'control + arrow keys'" })
+keymap.set('n', '<C-right>', ':vertical resize -4<CR>', { desc = "resize splits with 'control + arrow keys'" })
+keymap.set('n', '<C-up>', ':horizontal resize +4<CR>', { desc = "resize splits with 'control + arrow keys'" })
+keymap.set('n', '<C-down>', ':horizontal resize -4<CR>', { desc = "resize splits with 'control + arrow keys'" })
+-- LSP 
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = {buffer = event.buf}
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end,
+})
 -- nvim-tree
 require('nvim-tree').setup()
 keymap.set('n', '<leader>ft', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 
 -- fuzzy finder
-local telescope = require('telescope.builtin')
-keymap.set('n', '<leader>fb', telescope.buffers, { desc = "fuzzy find buffers" })
-keymap.set('n', '<leader>fh', telescope.help_tags, { desc = "shows help info for a bunch of shit you have installed" })
-keymap.set('n', '<leader>ff', function() telescope.find_files({hidden = true}) end, {desc = "fuzzy find files"})
-keymap.set('n', '<leader>fg', function() telescope.find_files({search_dirs= {"/home/lucky"}, hidden = true}) end, {desc = "fuzzy find from home"})
+require('fzf-lua').setup({ defaults = 'border-fused' })
+keymap.set('n', '<leader>ff', ':FzfLua files', { desc = "fuzzy find files" })
+keymap.set('n', '<leader>fg', ':FzfLua files cwd=/home/lucky', { desc = "fuzzy find files from home directory" })
+keymap.set('n', '<leader>fb', ':FzfLua buffers', { desc = "fuzzy find open buffers" })
+keymap.set('n', '<leader>gg', ':FzfLua grep', { desc = "fuzzy find using grep" })
 
 -- Auto-session
 keymap.set('n', '<leader>sr', ":SessionRes ore<CR>", { desc = "Restore session of cwd if any" })
@@ -143,13 +132,43 @@ keymap.set('n', '<leader>sw', ":SessionSave<CR>", { desc = "Save current session
 keymap.set('n', '<leader>ss', ":SessionSearch<CR>", { desc = "Search for open sessions (uses telescope by default)" })
 opt.sessionoptions:append('localoptions')
 
--- Mason setup (auto-install LSP servers)
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = {
-        "clangd", "pyright", "rust_analyzer", "lua_ls", "bashls", "jsonls", "yamlls", "ts_ls", "html", "cssls", "glsl_analyzer"
-    }
+-- disable terminal numbers in terminal mode
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.number = false            -- Disable absolute line numbers
+        vim.opt_local.relativenumber = false    -- Disable relative line numbers
+    end,
 })
+
+-- ======================================================================================================
+-- = PLUGIN CONFIGURATION ===============================================================================
+-- ======================================================================================================
+
+-- COLORSCHEME => https://github.com/nvim-treesitter/nvim-treesitter/wiki/Colorschemes
+vim.o.background =  'dark'
+vim.cmd.syntax "off"
+vim.cmd.colorscheme "vscode"
+
+-- LUALINE COLORSCHEME
+require('lualine').setup({options = {theme = 'vscode'}})
+
+-- CODE WINDOW
+local codewindow = require('codewindow')
+codewindow.setup()
+codewindow.apply_default_keybinds()
+
+-- TREESITTER
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { "c", "cpp", "python", "javascript", "scala", "rust", "bash", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json" }, -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    sync_install = false,
+    auto_install = true,
+    highlight = { enable = true }
+}
+
+-- MASON => https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+require("mason").setup()
+require("mason-lspconfig").setup({ ensure_installed = { "clangd", "pyright", "rust_analyzer", "lua_ls", "bashls", "jsonls", "html", "cssls", "vtsls" } })
 
 -- LSP CONFIGURATION
 local lspconfig = require("lspconfig")
@@ -158,39 +177,19 @@ lspconfig.rust_analyzer.setup({})
 lspconfig.lua_ls.setup({settings = {Lua = {diagnostics = {globals = {'vim'}}}}})
 lspconfig.bashls.setup({})
 lspconfig.jsonls.setup({})
-lspconfig.yamlls.setup({})
-lspconfig.ts_ls.setup({})
-lspconfig.vtsls.setup({})
 lspconfig.html.setup({})
 lspconfig.cssls.setup({})
+lspconfig.vtsls.setup({})
 lspconfig.pyright.setup({
     settings = {
         python = {
-            --pythonPath = vim.fn.exepath('python')
-            --pythonPath = '/home/lucky/Documents/classes/ai/ai_env/bin/python'
-            pythonPath = '/home/lucky/Documents/classes/ai/ai_env/bin/python3',  -- Set path to your virtual environment Python
+            pythonPath = '/hom/lucky/Documents/classes/info/info_env/bin/python' --pythonPath = vim.fn.exepath('python') --pythonPath = '/home/lucky/Documents/classes/ai/ai_env/bin/python' --pythonPath = '/home/lucky/Documents/classes/ai/ai_env/bin/python3', 
         },
-        analysis = {
-            extraPaths = { '/home/lucky/Documents/classes/ai/ai_env/lib/python3.11/site-packages' },  -- Add site-packages directory
+        analysis = { --extraPaths = { '/home/lucky/Documents/classes/ai/ai_env/lib/python3.11/site-packages' },  -- Add site-packages directory
             autoSearchPaths = true,
             diagnosticMode = "openFilesOnly",
             useLibraryCodeForTypes = true
         }
     }
 })
-
-
--- TreeSitter (syntax highlighting and indentation)
-require('nvim-treesitter.configs').setup {
-    ensure_installed = { "c", "cpp", "python", "javascript", "scala", "rust", "bash", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json" }, -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
-    auto_install = true,
-    highlight = {
-        enable = true,
-    }
-}
-
-
---vim.keymap.set('n', '<leader>d', vim.lsp.code_action, {})
---vim.keymap.set('n', '<leader>c', vim.lsp.hover, {})
 
