e38f6ea116aa197bb12e4bfa6a56849ee8c354a7
lucky <me@toaster.com>
2025-03-24 12:42:45 +0100

    added gitsign

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 -- leader
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"         -- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim
+vim.g.maplocalleader = "\\" -- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim
 -- tabs
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -19,20 +19,20 @@ opt.smarttab = true
 -- line numbers
 opt.number = true
 opt.relativenumber = true
-opt.cursorline = true               -- highlight current cursor line
+opt.cursorline = true              -- highlight current cursor line
 -- search settings
-opt.ignorecase = true               -- case insensitive search
-opt.smartcase = true                -- assumes case sensitive if mixed case in search
+opt.ignorecase = true              -- case insensitive search
+opt.smartcase = true               -- assumes case sensitive if mixed case in search
 -- clipboard, mouse support
-opt.clipboard = 'unnamedplus'       -- Set up the clipboard YOU NEED TO HAVE XCLIP INSTALLED
-opt.mouse = 'a'                     -- Enable mouse support
+opt.clipboard = 'unnamedplus'      -- Set up the clipboard YOU NEED TO HAVE XCLIP INSTALLED
+opt.mouse = 'a'                    -- Enable mouse support
 -- other
-opt.laststatus = 2                  -- always show the status line
-opt.wrap = false                    -- same as nowrap
-opt.showmatch = true                -- highlight matching parenthesis
-opt.signcolumn = "yes"              -- show sign column so that text doesn't shift
-opt.backspace = "indent,eol,start"  -- allow backspace on any of these
-opt.scrolloff = 15                  -- when scrolling, prevent cursor from being at first or last line
+opt.laststatus = 2                 -- always show the status line
+opt.wrap = false                   -- same as nowrap
+opt.showmatch = true               -- highlight matching parenthesis
+opt.signcolumn = "yes"             -- show sign column so that text doesn't shift
+opt.backspace = "indent,eol,start" -- allow backspace on any of these
+opt.scrolloff = 15                 -- when scrolling, prevent cursor from being at first or last line
 opt.sidescrolloff = 30
 opt.splitright = true
 
@@ -48,11 +48,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
+            { out,                            "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
-    os.exit(1)
+        os.exit(1)
     end
 end
 opt.rtp:prepend(lazypath)
@@ -60,29 +60,30 @@ opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
     spec = {
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
-        {'neovim/nvim-lspconfig', lazy = false},
-        {'nvim-treesitter/nvim-treesitter'},
-        {'Mofiqul/vscode.nvim'},
-        {"ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" }}, --, opts = {}},
-        {'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}},
-        {'rmagatti/auto-session', lazy = false, opts = {suppressed_dirs = {'~/Downloads', '/'}}},
-        {'nvim-tree/nvim-tree.lua', lazy = false, dependencies = { 'nvim-tree/nvim-web-devicons'}},
-        {'gorbit99/codewindow.nvim'}, -- leader mm (minimap toggle) leader mf (minimap focus)
+        { 'williamboman/mason.nvim' },
+        { 'williamboman/mason-lspconfig.nvim' },
+        { 'neovim/nvim-lspconfig',            lazy = false },
+        { 'nvim-treesitter/nvim-treesitter' },
+        { 'lewis6991/gitsigns.nvim' },
+        { 'Mofiqul/vscode.nvim' },
+        { "ibhagwan/fzf-lua",                 dependencies = { "nvim-tree/nvim-web-devicons" } }, --, opts = {}},
+        { 'nvim-lualine/lualine.nvim',        dependencies = { 'nvim-tree/nvim-web-devicons' } },
+        { 'rmagatti/auto-session',            lazy = false,                                    opts = { suppressed_dirs = { '~/Downloads', '/' } } },
+        { 'nvim-tree/nvim-tree.lua',          lazy = false,                                    dependencies = { 'nvim-tree/nvim-web-devicons' } },
+        { 'gorbit99/codewindow.nvim' },       -- leader mm (minimap toggle) leader mf (minimap focus)
     },
-    install = { colorscheme = { "vscode" } },    -- colorscheme that will be used when installing plugins.
-    checker = { enabled = true },                -- automatically check for plugin updates
+    install = { colorscheme = { "vscode" } }, -- colorscheme that will be used when installing plugins.
+    checker = { enabled = true },             -- automatically check for plugin updates
 })
 
 -- ======================================================================================================
 -- = KEYMAPS ============================================================================================
 -- ======================================================================================================
 
-keymap.set("i", "df", "<ESC>", { desc = "Exit insert mode with 'df'"})
+keymap.set("i", "df", "<ESC>", { desc = "Exit insert mode with 'df'" })
 keymap.set('t', 'df', '<C-\\><C-n>', { desc = "Exit insert mode when in terminal mode" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "no highlight, clear search highlights"})
--- BUFFERS 
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "no highlight, clear search highlights" })
+-- BUFFERS
 keymap.set('n', '<leader>n', ':enew<CR>', { desc = "open a new empty buffer" })
 keymap.set('n', '<leader>w', ':bd!<CR>', { desc = "Close / delete current buffer" })
 keymap.set('n', '<leader>t', ':term<CR>', { desc = "Open a terminal buffer" })
@@ -98,11 +99,11 @@ keymap.set('n', '<C-left>', ':vertical resize +4<CR>', { desc = "resize splits w
 keymap.set('n', '<C-right>', ':vertical resize -4<CR>', { desc = "resize splits with 'control + arrow keys'" })
 keymap.set('n', '<C-up>', ':horizontal resize +4<CR>', { desc = "resize splits with 'control + arrow keys'" })
 keymap.set('n', '<C-down>', ':horizontal resize -4<CR>', { desc = "resize splits with 'control + arrow keys'" })
--- LSP 
+-- LSP
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
-        local opts = {buffer = event.buf}
+        local opts = { buffer = event.buf }
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
         vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
@@ -111,20 +112,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
         vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
         vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
     end,
 })
 -- nvim-tree
 require('nvim-tree').setup()
-keymap.set('n', '<leader>ft', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+keymap.set('n', '<leader>ft', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
--- fuzzy finder
-require('fzf-lua').setup({ defaults = 'border-fused' })
-keymap.set('n', '<leader>ff', ':FzfLua files', { desc = "fuzzy find files" })
-keymap.set('n', '<leader>fg', ':FzfLua files cwd=/home/lucky', { desc = "fuzzy find files from home directory" })
-keymap.set('n', '<leader>fb', ':FzfLua buffers', { desc = "fuzzy find open buffers" })
-keymap.set('n', '<leader>gg', ':FzfLua grep', { desc = "fuzzy find using grep" })
+-- FZF-LUA
+require('fzf-lua').setup({ 'border-fused' })
+keymap.set('n', '<leader>ff', '<Cmd>FzfLua files<CR>', { desc = "fuzzy find files from CWD" })
+keymap.set('n', '<leader>fh', '<Cmd>FzfLua files cwd=/home/lucky/<CR>', { desc = "fuzzy find files from ~" })
+keymap.set('n', '<leader>fb', '<Cmd>FzfLua buffers<CR>', { desc = "fuzzy find open buffers" })
+keymap.set('n', '<leader>fg', '<Cmd>FzfLua grep<CR>', { desc = "fuzzy find using grep" })
 
 -- Auto-session
 keymap.set('n', '<leader>sr', ":SessionRes ore<CR>", { desc = "Restore session of cwd if any" })
@@ -132,12 +133,21 @@ keymap.set('n', '<leader>sw', ":SessionSave<CR>", { desc = "Save current session
 keymap.set('n', '<leader>ss', ":SessionSearch<CR>", { desc = "Search for open sessions (uses telescope by default)" })
 opt.sessionoptions:append('localoptions')
 
+-- GITSIGNS 
+keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', { desc = "view changed lines" })
+keymap.set('n', '<leader>gb', ':Gitsigns stage_buffer<CR>', { desc = "view changed lines" })
+keymap.set('n', '<leader>gp', '<Cmd>Gitsigns preview_hunk_inline<CR>', { desc = "view changed lines" })
+keymap.set('n', '<leader>gk', '<Cmd>Gitsigns prev_hunk<CR><Cmd>Gitsigns preview_hunk_inline<CR>', { desc = "view changed lines" })
+keymap.set('n', '<leader>gj', '<Cmd>Gitsigns next_hunk<CR><Cmd>Gitsigns preview_hunk_inline<CR>', { desc = "view changed lines" })
+keymap.set('n', '<leader>gh', '<Cmd>Gitsigns stage_hunk<CR>', { desc = "view changed lines" })
+keymap.set('n', '<leader>gu', '<Cmd>Gitsigns undo_stage_hunk<CR>', { desc = "view changed lines" })
+
 -- disable terminal numbers in terminal mode
 vim.api.nvim_create_autocmd("TermOpen", {
     pattern = "*",
     callback = function()
-        vim.opt_local.number = false            -- Disable absolute line numbers
-        vim.opt_local.relativenumber = false    -- Disable relative line numbers
+        vim.opt_local.number = false         -- Disable absolute line numbers
+        vim.opt_local.relativenumber = false -- Disable relative line numbers
     end,
 })
 
@@ -146,12 +156,12 @@ vim.api.nvim_create_autocmd("TermOpen", {
 -- ======================================================================================================
 
 -- COLORSCHEME => https://github.com/nvim-treesitter/nvim-treesitter/wiki/Colorschemes
-vim.o.background =  'dark'
+vim.o.background = 'dark'
 vim.cmd.syntax "off"
 vim.cmd.colorscheme "vscode"
 
 -- LUALINE COLORSCHEME
-require('lualine').setup({options = {theme = 'vscode'}})
+require('lualine').setup({ options = { theme = 'vscode' } })
 
 -- CODE WINDOW
 local codewindow = require('codewindow')
@@ -166,6 +176,9 @@ require('nvim-treesitter.configs').setup {
     highlight = { enable = true }
 }
 
+-- GIT SIGNS
+require('gitsigns').setup()
+
 -- MASON => https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
 require("mason").setup()
 require("mason-lspconfig").setup({ ensure_installed = { "clangd", "pyright", "rust_analyzer", "lua_ls", "bashls", "jsonls", "html", "cssls", "vtsls" } })
@@ -174,22 +187,21 @@ require("mason-lspconfig").setup({ ensure_installed = { "clangd", "pyright", "ru
 local lspconfig = require("lspconfig")
 lspconfig.clangd.setup({})
 lspconfig.rust_analyzer.setup({})
-lspconfig.lua_ls.setup({settings = {Lua = {diagnostics = {globals = {'vim'}}}}})
 lspconfig.bashls.setup({})
 lspconfig.jsonls.setup({})
 lspconfig.html.setup({})
 lspconfig.cssls.setup({})
 lspconfig.vtsls.setup({})
-lspconfig.pyright.setup({
+lspconfig.lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { 'vim' } } } } })
+lspconfig.pyright.setup({})
+--[[
     settings = {
-        python = {
-            pythonPath = '/hom/lucky/Documents/classes/info/info_env/bin/python' --pythonPath = vim.fn.exepath('python') --pythonPath = '/home/lucky/Documents/classes/ai/ai_env/bin/python' --pythonPath = '/home/lucky/Documents/classes/ai/ai_env/bin/python3', 
-        },
-        analysis = { --extraPaths = { '/home/lucky/Documents/classes/ai/ai_env/lib/python3.11/site-packages' },  -- Add site-packages directory
+        python = { pythonPath = vim.fn.exepath('python') },
+        analysis = {
             autoSearchPaths = true,
             diagnosticMode = "openFilesOnly",
             useLibraryCodeForTypes = true
         }
     }
 })
-
+--]]
