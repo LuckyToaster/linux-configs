58765ed3415fab14cfd53538226495f9473a92f9
lucky <me@toaster.com>
2025-05-01 16:22:50 +0200

    updated config

--- nvim/lua/pluginConfigs.lua
+++ nvim/lua/pluginConfigs.lua
@@ -37,22 +37,22 @@ require('lualine').setup({
 })
 
 -- NOICE
-require("noice").setup({
-    lsp = {
-        override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-    },
-    presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- (i dont have inc rename plugin so dont care)
-        lsp_doc_border = true, -- this is nice
-    },
-})
+-- require("noice").setup({
+--     lsp = {
+--         override = {
+--             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--             ["vim.lsp.util.stylize_markdown"] = true,
+--             ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+--         },
+--     },
+--     presets = {
+--         bottom_search = false, -- use a classic bottom cmdline for search
+--         command_palette = true, -- position the cmdline and popupmenu together
+--         long_message_to_split = true, -- long messages will be sent to a split
+--         inc_rename = false, -- (i dont have inc rename plugin so dont care)
+--         lsp_doc_border = true, -- this is nice
+--     },
+-- })
 
 
 -- CODE WINDOW
--- nvim/lua/pluginManager.lua
+++ nvim/lua/pluginManager.lua
@@ -28,13 +28,37 @@ require("lazy").setup({
         { "ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
         { 'nvim-tree/nvim-tree.lua', lazy = false, dependencies = { 'nvim-tree/nvim-web-devicons' } },
         { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
-        { "folke/noice.nvim", event = "VeryLazy", dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" } },
+        --{ "folke/noice.nvim", event = "VeryLazy", dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" } },
         -- other nice stuff
         { 'lewis6991/gitsigns.nvim' }, -- git stuff
         { 'Mofiqul/vscode.nvim' }, -- colorscheme
         { 'gorbit99/codewindow.nvim' }, -- leader mm (minimap toggle) leader mf (minimap focus)
         { 'MeanderingProgrammer/render-markdown.nvim', dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' } },
         --{ "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+        { "folke/which-key.nvim", event = "VeryLazy", keys = {{"<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)"}}},
+        {
+          "sphamba/smear-cursor.nvim",
+
+          opts = {
+            -- Smear cursor when switching buffers or windows.
+            smear_between_buffers = true,
+
+            -- Smear cursor when moving within line or to neighbor lines.
+            -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+            smear_between_neighbor_lines = true,
+
+            -- Draw the smear in buffer space instead of screen space when scrolling
+            scroll_buffer_space = true,
+
+            -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+            -- Smears will blend better on all backgrounds.
+            legacy_computing_symbols_support = false,
+
+            -- Smear cursor in insert mode.
+            -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+            smear_insert_mode = true,
+          },
+        }
     },
     install = { colorscheme = { "vscode" } }, -- colorscheme that will be used when installing plugins.
     checker = { enabled = true },             -- automatically check for plugin updates
